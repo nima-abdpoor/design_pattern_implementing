a8c5e764a70581d77cf1021e3d63f69c68106c3b
nima-abdpoor <nimaabdi328@yahoo.com>
2021-03-02 09:22:26 +0330

    6. Adapter pattern

--- src/com/company/main/Main.java
+++ src/com/company/main/Main.java
@@ -12,9 +12,10 @@ import com.company.FactoryMethodPattern.Factory.RandomAnimalFactory;
 import com.company.Singleton.Singleton;
 import com.company.abstractFactoryPattern.ThemeFactory.DarkTheme;
 import com.company.abstractFactoryPattern.ThemeFactory.LightTheme;
-import com.company.decoratorpattern.Beverage.Beverage;
-import com.company.decoratorpattern.Beverage.Espresso;
-import com.company.decoratorpattern.Beverage.Tea;
+import com.company.adapter.RoundHole;
+import com.company.adapter.RoundPeg;
+import com.company.adapter.SquarePeg;
+import com.company.adapter.SquarePegAdapter;
 import com.company.ObserverPattern.observable.WeatherStation;
 import com.company.ObserverPattern.observers.PhoneDisplay;
 import com.company.strategy_pattern.Father.Duck;
@@ -23,13 +24,37 @@ import com.company.strategy_pattern.children.CityDuck;
 public class Main {
 
     public static void main(String[] args) {
-        //AbstractFactoryPattern();
+        //abstractFactoryPattern();
         //FactoryMethodPattern();
-        //strategypattern();
+        //strategyPattern();
         //observerPattern();
         //decoratorPattern();
         //singleton();
-        CommandPattern();
+        //CommandPattern();
+        AdapterPattern();
+    }
+
+    private static void AdapterPattern() {
+        // Round fits round, no surprise.
+        RoundHole hole = new RoundHole(5);
+        RoundPeg rpeg = new RoundPeg(5);
+        if (hole.fits(rpeg)) {
+            System.out.println("Round peg r5 fits round hole r5.");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+        // hole.fits(smallSqPeg); // Won't compile.
+
+        // Adapter solves the problem.
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
     }
 
     private static void CommandPattern() {
@@ -51,7 +76,7 @@ public class Main {
         System.out.println(singleton.testString);
     }
 
-    private static void AbstractFactoryPattern() {
+    private static void abstractFactoryPattern() {
         DarkTheme darkTheme = new DarkTheme();
         System.out.println("DARK:");
         System.out.println(darkTheme.TEXT_COLOR().getColor());
@@ -94,7 +119,7 @@ public class Main {
         station.Notify();
     }
 
-    public static void strategypattern() {
+    public static void strategyPattern() {
         CityDuck cityDuck = new CityDuck();
         Duck duck = new Duck(cityDuck, cityDuck, cityDuck, cityDuck);
         duck.fly();
